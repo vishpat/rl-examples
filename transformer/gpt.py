7153e0d62723c16614e2f32f265d752c0ab07e01
Vishal Patil <vishpat@gmail.com>
2026-02-10 11:22:48 -0500

    Lets build a gpt

---
+++ transformer/gpt.py
@@ -0,0 +1,59 @@
+import requests
+import os
+import torch
+from torch.utils.data import Dataset
+
+def download_shakespeare():
+    """Download the tiny Shakespeare dataset"""
+    url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+    
+    if not os.path.exists('shakespeare.txt'):
+        print("Downloading Shakespeare dataset...")
+        response = requests.get(url)
+        with open('shakespeare.txt', 'w') as f:
+            f.write(response.text)
+    
+    with open('shakespeare.txt', 'r') as f:
+        text = f.read()
+    
+    return text
+
+class CharTokenizer:
+    """Simple character-level tokenizer"""
+    
+    def __init__(self, text):
+        self.chars = sorted(list(set(text)))
+        self.vocab_size = len(self.chars)
+        self.char_to_idx = {ch: i for i, ch in enumerate(self.chars)}
+        self.idx_to_char = {i: ch for i, ch in enumerate(self.chars)}
+    
+    def encode(self, text):
+        return [self.char_to_idx[ch] for ch in text]
+    
+    def decode(self, indices):
+        return ''.join([self.idx_to_char[i] for i in indices])
+
+class ShakespeareDataset(Dataset):
+    """Dataset for autoregressive language modeling"""
+    
+    def __init__(self, text, tokenizer, block_size=128):
+        self.tokenizer = tokenizer
+        self.block_size = block_size
+        self.data = torch.tensor(tokenizer.encode(text), dtype=torch.long)
+    
+    def __len__(self):
+        return len(self.data) - self.block_size
+    
+    def __getitem__(self, idx):
+        # Input sequence and target (shifted by 1)
+        x = self.data[idx:idx + self.block_size]
+        y = self.data[idx + 1:idx + self.block_size + 1]
+        return x, y
+
+if __name__ == "__main__":
+    tokenizer = CharTokenizer(download_shakespeare())
+    dataset = ShakespeareDataset(download_shakespeare(), tokenizer)
+    for x, y in dataset:
+        print(x)
+        print(y)
+        break
